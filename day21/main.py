50f392421679b9c9057a3b0ed514702eea3e2976
Jesse Capper-Ream <jessecapper@gmail.com>
2022-12-20 23:11:33 -0800

    day 21 completed

---
+++ day21/main.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+from operator import add, floordiv, mul, sub
+
+operator_mapping = {'+': add, '*': mul, '-': sub, '/': floordiv}
+with open('input.txt', 'r') as fd:
+    monkeys = {}
+    for line in fd:
+        parts = line.replace(':', '').split()
+        if len(parts) == 2:
+            monkeys[parts[0]] = int(parts[1])
+        else:
+            monkeys[parts[0]] = parts[1:4]
+
+
+def get_value(monkeys, node, variable=None):
+    value = monkeys[node]
+    if isinstance(value, int):
+        return value
+    elif isinstance(value, str):
+        return f'({value})'
+
+    lside, opstr, rside = value
+    lval = variable if lside == variable else get_value(monkeys, lside, variable=variable)
+    rval = variable if rside == variable else get_value(monkeys, rside, variable=variable)
+    if isinstance(lval, str) or isinstance(rval, str):
+        monkeys[node] = f'{lval} {opstr} {rval}'
+    else:
+        monkeys[node] = operator_mapping[opstr](lval, rval)
+
+    return get_value(monkeys, node)
+
+
+print('Part 1:', int(get_value(deepcopy(monkeys), 'root')))
+
+m2 = deepcopy(monkeys)
+m2['root'][1] = '='
+equation = get_value(m2, 'root', variable='humn')
+
+# Not going to pretend like I know why this works
+expr = equation.replace("=", " - (") + ")"
+grouped = eval(expr.replace('humn', '1j'))
+print('Part 2:', int(round(-grouped.real / grouped.imag)))
